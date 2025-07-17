691c14ab6dc74e4b7f132bc3166651040119e137
Nhan Ngo <tnt1232007@gmail.com>
2025-07-17 20:06:55 +0700

    Add traefik-proxy call to docker functions

--- linux-03-bash-aliases.sh
+++ linux-03-bash-aliases.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
 ALIASES=$(cat << 'EOF'
-# docker / docker compose
+# docker functions/aliases
 function dcuuf() {
-    echo "🚀 Upgrading $1 to $2";
-    sed -i -E "s/image:(.*):.*/\image:\1:$2/" ./$1/docker-compose.yml;
     export HOSTNAME;
-    docker compose -f ./$1/docker-compose.yml up -d;
+    export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
+    echo "⬆️ Upgrading $SESSION_DOCKER_NAME to $2";
+    sed -i -E "s/image:(.*):.*/\image:\1:$2/" ./$SESSION_DOCKER_NAME/docker-compose.yml;
+    docker compose -f ./$SESSION_DOCKER_NAME/docker-compose.yml up -d --remove-orphans;
 }
 function dcpf() {
+    export HOSTNAME;
     export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
     echo "📦 Pulling images for $SESSION_DOCKER_NAME";
-    export HOSTNAME;
     docker compose -f ./$SESSION_DOCKER_NAME/docker-compose.yml pull;
 }
 function dcuf() {
+    export HOSTNAME;
     export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
     echo "🚀 Bringing up $SESSION_DOCKER_NAME";
-    export HOSTNAME;
     docker compose -f ./$SESSION_DOCKER_NAME/docker-compose.yml up -d --remove-orphans;
+    curl https://auto.trinitro.io/webhook/traefik-proxy
 }
 function dcdf() {
+    export HOSTNAME;
     export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
     echo "🛑 Bringing down $SESSION_DOCKER_NAME";
-    export HOSTNAME;
     docker compose -f ./$SESSION_DOCKER_NAME/docker-compose.yml down -t 120;
+    curl https://auto.trinitro.io/webhook/traefik-proxy
 }
 function dcrf() {
+    export HOSTNAME;
     export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
     echo "🔄 Restarting $SESSION_DOCKER_NAME";
-    export HOSTNAME;
     docker compose -f ./$SESSION_DOCKER_NAME/docker-compose.yml restart -t 120;
 }
 function dclf() {
+    export HOSTNAME;
     export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
     echo "📜 Following logs for $SESSION_DOCKER_NAME";
     docker logs $SESSION_DOCKER_NAME --follow;
 }
 function dcef() {
+    export HOSTNAME;
     export SESSION_DOCKER_NAME="${1:-$SESSION_DOCKER_NAME}"
     echo "🔧 Cracking into $SESSION_DOCKER_NAME ${2-bash}";
     docker exec -it $SESSION_DOCKER_NAME /bin/${2-bash}
 }
-alias dcu='docker compose up -d'
-alias dcd='docker compose down -t 120'
 alias dip='docker image prune -af'
-alias diu='docker inspect $(docker ps -q) --format "{{.Config.User}} {{.Name}}"'
 
 EOF
 )
@@ -55,4 +57,4 @@ fi'
 grep -qxF "$ALIAS_SNIPPET" ~/.bashrc || echo "$ALIAS_SNIPPET" >> ~/.bashrc
 source ~/.bashrc
 # For synology or any system use old version of docker-compose
-# sed -i 's/docker compose/docker-compose/g' ~/.bash_aliases 
+# sed -i 's/docker compose/docker-compose/g' ~/.bash_aliases
